673ca80b1aed9c033c27eddc5333f487635c0a42
junki789 <junkie0915@gmail.com>
2019-11-20 18:32:17 +0900

    syuusei2

--- mercari/config/deploy.rb
+++ mercari/config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_63a_nagoya/mercari'
+set :application, 'freemarket_sample_63a_nagoya'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:junki789/freemarket_sample_63a_nagoya.git'
