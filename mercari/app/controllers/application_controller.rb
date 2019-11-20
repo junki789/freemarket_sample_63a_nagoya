df158c2f1818f583f06257ae4c8c7e23834de7e3
junki789 <junkie0915@gmail.com>
2019-11-20 16:21:52 +0900

    deproy3

--- mercari/app/controllers/application_controller.rb
+++ mercari/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'aaaa' && password == '1111'
+      username == 'bbbb' && password == '1111'
     end
   end
 end
