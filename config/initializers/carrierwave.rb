6644f51d66aeba6536614a925a9e6525ea35437a
junki789 <junkie0915@gmail.com>
2019-11-21 12:41:55 +0900

    S3条件分岐3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,17 +3,16 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+  if Rails.env.development? || Rails.env.test?
     config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
+  elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: 'AWS',     #AWSのアクセスキーとシークレットキーを環境変数で定義する
+      provider: 'AWS',
       aws_access_key_id: Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region: #'AWSで設定した地域（おそらく'ap-northeast-1') '
-    }
+      region:'ap-northeast-1'
     config.fog_directory  = 'takarabako5353'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takarabako5353'
   end
