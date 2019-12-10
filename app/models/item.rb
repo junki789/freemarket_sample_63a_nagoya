fa2921fba6a5ffbd6f78ec69a06dd3af71a8dd8a
taka0116 <taka0116ii@icloud.com>
2019-12-10 16:58:44 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :item_images, dependent: :destroy
   has_many :users, through: :sellers
-  belongs_to :size_type
+  # belongs_to :size_type
   accepts_nested_attributes_for :item_images, allow_destroy: true
   #belongs_to :user
-  #belongs_to :size_type
+  
 end
