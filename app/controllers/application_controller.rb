6a110df546e7ba38a73a8a5d62eddd4fafb82a46
koutarou893101 <56900895+koutarou893101@users.noreply.github.com>
2019-12-12 20:04:10 +0900

    ログイン画面の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :basic_auth
-  #before_action :configure_permitted_parameters, if: :devise_controller?
-
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource) 
+    root_path
+  end
 
   private
+  
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
